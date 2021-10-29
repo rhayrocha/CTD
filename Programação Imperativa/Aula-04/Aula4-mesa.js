ece59235930993c7f2997520d54eeaf6e521a3bb
Rhay Rocha <68596197+rhayrocha@users.noreply.github.com>
2021-10-28 21:26:39 -0300

    Delete Programação Imperativa directory

--- Programação Imperativa/Aula-04/Aula4-mesa.js
+++
@@ -1,7 +0,0 @@
-let nome = "Carlos de Souza";
-let idade = 28;
-let peso = 80.1;
-let altura = 1.76;
-let plano = false;
-const imc = peso / (altura * altura);
-console.log(nome + " tem " + idade + " anos e seu índice de massa corporal é de " + imc);
